82bdd8ffa0f23bc8b7d0f3bfc6a39bd9d3f49701
thomashummler <72397706+thomashummler@users.noreply.github.com>
2024-01-09 09:57:54 +0000

    checking out

--- Hello.py
+++ Hello.py
@@ -1,15 +1,21 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 import os
 
 API_KEY = os.environ["API_KEY"]
 
-openai.api_key = API_KEY
+
 
 input = st.text_input("Ask a question")
 
-if st.button("Submit"):
 
-    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": input}])
+if st.button("Submit"):
+    client = OpenAI(
+    api_key= API_KEY
+    )
+    chat_completion = client.chat.completions.create(
+         model="gpt-4-1106-preview",
+        messages=[{"role": "user", "content": input}]
+        )
 
     st.write(chat_completion.choices[0].message.content)
